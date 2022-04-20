2768c2f2b84ac606a2a59764f23b0f0247c0b65f
ancar <ancar@AndresPC>
2022-04-19 19:22:46 -0600

    Examen02

---
+++ src/main/java/ANDRESCARTINUGALDE_II_EXAMEN_PG/demo/AndrescartinugaldeIiExamenPgApplication.java
@@ -0,0 +1,13 @@
+package ANDRESCARTINUGALDE_II_EXAMEN_PG.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrescartinugaldeIiExamenPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndrescartinugaldeIiExamenPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ANDRESCARTINUGALDE_II_EXAMEN_PG/demo/AndrescartinugaldeIiExamenPgApplicationTests.java
@@ -0,0 +1,13 @@
+package ANDRESCARTINUGALDE_II_EXAMEN_PG.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndrescartinugaldeIiExamenPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
